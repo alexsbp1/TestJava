1379ac421fc17bb35c0e8181ea0ceeaa6579d4b9
bysted <alexsbp@outlook.com>
2020-11-19 16:42:24 +0100

    added frame.java and added 1 unit test in AppTest.java

--- unitTest/src/test/java/jar/AppTest.java
+++ unitTest/src/test/java/jar/AppTest.java
@@ -2,12 +2,28 @@ package jar;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
 
 /**
  * Unit test for simple App.
  */
-class AppTest {
+public class AppTest {
+    
+    private static Frame frame; 
+    @BeforeAll
+    public static void initializeFrame () {
+        frame = new Frame();
+    }
+    
+    @Test
+	public void testIsStrike() throws Exception {
+		frame.rollFirst(10);
+		assertTrue(frame.isStrike());
+	}
+    
     /**
      * Rigorous Test.
      */
